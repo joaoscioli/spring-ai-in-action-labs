442471cd5515565cab2f705825bb897bbd8c5045
joaoscioli <joaoscioli@outlook.com>
2025-11-14 10:42:19 -0300

    feat(01-getting-started) add new project chapter 01 spring IA

---
+++ board-game-buddy/src/main/groovy/com/example/boardgamebuddy/BoardGameBuddyApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.boardgamebuddy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class BoardGameBuddyApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(BoardGameBuddyApplication, args)
+    }
+
+}
